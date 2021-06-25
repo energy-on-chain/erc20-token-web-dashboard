c4d7b14fcf483fdfa1a03e49e73f0f53ae32d293
Matthew Hartigan <matthew.t.hartigan@gmail.com>
2021-06-25 15:17:41 -0500

    Implemented makeOrder, cancelOrder, fillOrder, _trade functions and associated test cases.

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -10,8 +10,8 @@
 // [x] Deposit tokens
 // [x] Withdraw tokens
 // [x] Check balances
-// [] Make order
-// [] Cancel order
+// [x] Make order
+// [x] Cancel order
 // [] Fill order
 // [] Charge fees
 
@@ -28,6 +28,22 @@ contract Exchange {
 	uint256 public feePercent;    // the fee percentage
 	address constant ETHER = address(0);    // store Ether in tokens mapping with blank address
 	mapping(address => mapping(address => uint256)) public tokens;    // maps all deposited tokens to all users who have deposited them
+	mapping(uint256 => _Order) public orders;    // order book
+	uint256 public orderCount;
+	mapping(uint256 => bool) public orderCancelled;
+	mapping(uint256 => bool) public orderFilled;
+
+
+	// STRUCTS
+	struct _Order {
+		uint256 id;
+		address user;
+		address tokenGet;
+		uint256 amountGet;
+		address tokenGive;
+		uint256 amountGive;
+		uint256 timestamp;
+	}
 
 
 	// CONSTRUCTORS
@@ -40,6 +56,9 @@ contract Exchange {
 	// EVENTS
 	event Deposit(address token, address user, uint256 amount, uint256 balance);
 	event Withdraw(address token, address user, uint256 amount, uint256 balance);
+	event Order(uint256 id, address user, address tokenGet, uint256 amountGet, address tokenGive, uint256 amountGive, uint256 timestamp);
+	event Cancel(uint256 id, address user, address tokenGet, uint256 amountGet, address tokenGive, uint256 amountGive, uint256 timestamp);
+	event Trade(uint256 id, address user, address tokenGet, uint256 amountGet, address tokenGive, uint256 amountGive, address userFill, uint256 timestamp);
 
 
 	// FUNCTIONS
@@ -77,5 +96,44 @@ contract Exchange {
 
 	function balanceOf(address _token, address _user) public view returns (uint256) {
 		return tokens[_token][_user];
+	}
+
+	function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
+		orderCount = orderCount.add(1);
+		orders[orderCount] = _Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+		emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+	}
+
+	function cancelOrder(uint256 _id) public {
+		_Order storage _order = orders[_id];
+		require(address(_order.user) == msg.sender);
+		require(_order.id == _id);    // order must exist
+		orderCancelled[_id] = true;
+		emit Cancel(orderCount, msg.sender, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive, now);
+	}
+
+	function fillOrder(uint256 _id) public {
+		require(_id > 0 && _id <= orderCount);
+		require(!orderFilled[_id]);
+		require(!orderCancelled[_id]);
+		_Order storage _order = orders[_id];
+		_trade(_order.id, _order.user, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive);
+		orderFilled[_order.id] = true;
+	}
+
+	function _trade(uint256 _orderId, address _user, address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) internal {
+		// Fee paid by the user that fills the order, aka msg.sender
+		uint256 _feeAmount = _amountGive.mul(feePercent).div(100);
+
+
+		// Execute trade
+		tokens[_tokenGet][msg.sender] = tokens[_tokenGet][msg.sender].sub(_amountGet.add(_feeAmount));
+		tokens[_tokenGet][_user] = tokens[_tokenGet][_user].add(_amountGet);
+		tokens[_tokenGet][feeAccount] = tokens[_tokenGet][feeAccount].add(_feeAmount);
+		tokens[_tokenGive][_user] = tokens[_tokenGive][_user].sub(_amountGive);
+		tokens[_tokenGive][msg.sender] = tokens[_tokenGive][msg.sender].add(_amountGive);
+
+		// Emit trade event
+		emit Trade(_orderId, _user, _tokenGet, _amountGet, _tokenGive, _amountGive, msg.sender, now);
 	}
 }
