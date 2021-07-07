682ca3f129154603f19f0115d4144378b726369f
Matthew Hartigan <matthew.t.hartigan@gmail.com>
2021-07-06 22:36:00 -0500

    Created Trade.js component and connected trade data to it from blockchain/redux.  Next step is to format it into humman readable form.

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -1,7 +1,9 @@
-import { web3Loaded, web3AccountLoaded, tokenLoaded, exchangeLoaded } from './actions'
+import { web3Loaded, web3AccountLoaded, tokenLoaded, exchangeLoaded, cancelledOrdersLoaded, filledOrdersLoaded, allOrdersLoaded } from './actions'
 import Web3 from 'web3'
 import Token from '../abis/Token.json'
 import Exchange from '../abis/Exchange.json'
+
+// Interactions extract data from the connected blockchain
 
 export const loadWeb3 = (dispatch) => {
 	const connection = new Web3(Web3.givenProvider || 'http://localhost:7545')
@@ -36,4 +38,27 @@ export const loadExchange = async (web3, networkId, dispatch) => {
 		console.log('Contract not deployed to the current network.  Please select another network with Metamask.')
 		return null
 	}
+}
+
+export const loadAllOrders = async (exchange, dispatch) => {
+	// Fetch cancelled orders with the 'cancel' event stream
+	const cancelStream = await exchange.getPastEvents('Cancel', { fromBlock: 0, toBlock: 'latest'})
+	// Format cancelled orders
+	const cancelledOrders = cancelStream.map((event) => event.returnValues)
+	// Add cancelled orders to the redux store
+	dispatch(cancelledOrdersLoaded(cancelledOrders))
+	
+	// Fetch filled orders with the 'trade' event stream
+	const tradeStream = await exchange.getPastEvents('Trade', { fromBlock: 0, toBlock: 'latest'})
+	// Format filled orders
+	const filledOrders = tradeStream.map((event) => event.returnValues)
+	// Add filled orders to the redux store
+	dispatch(filledOrdersLoaded(filledOrders))
+
+	// Load orders stream
+	const orderStream = await exchange.getPastEvents('Order', { fromBlock: 0, toBlock: 'latest'})
+	// Format orders stream
+	const allOrders = orderStream.map((event) => event.returnValues)
+	// Add open orders to the redux store
+	dispatch(allOrdersLoaded(allOrders))
 }
--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,6 +1,8 @@
 import { get } from 'lodash'
 import { createSelector } from 'reselect'
 
+// Selectors extract data that is being tracked in the redux state so it can be inserted into the React app html
+
 const account = state => get(state, 'web3.account')
 export const accountSelector = new createSelector(account, a => a)
 
@@ -10,4 +12,35 @@ export const tokenLoadedSelector = new createSelector(tokenLoaded, tl => tl)
 const exchangeLoaded = state => get(state, 'exchange.loaded', false)
 export const exchangeLoadedSelector = new createSelector(exchangeLoaded, el => el)
 
+const exchange = state => get(state, 'exchange.contract')
+export const exchangeSelector = createSelector(exchange, e => e)
+
 export const contractsLoadedSelector = new createSelector(tokenLoaded, exchangeLoaded, (tl, el) => (tl && el))
+
+const filledOrdersLoaded = state => get(state, 'exchange.filledOrders.loaded', false)
+export const filledOrdersLoadedSelector = new createSelector(filledOrdersLoaded, loaded => loaded)
+
+const filledOrders = state => get(state, 'exchange.filledOrders.data', [])
+export const filledOrdersSelector = new createSelector(
+	filledOrders, 
+	(orders) => { 
+		// Decorate the orders
+		// TODO... fill me in
+
+		// Sort orders by date descending for display
+		orders = orders.sort((a, b) => b.timestamp - a.timestamp)
+		console.log(orders) 
+	}
+)
+
+const decorateFilledOrders = (orders) => {
+	return (
+		orders.map((order) => {
+			order = decorateOrder(order)
+		})
+	)
+}
+
+const decorateOrder = (order) => {
+	// TODO... fill me in
+}
